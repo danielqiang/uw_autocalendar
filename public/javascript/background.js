59b8f92b6dc504402abca8f9e18a90037dab62e4
Xiaoyue Sun <xiaoyuesun@XiaoyuedeMacBook-Pro.local>
2021-04-18 16:23:35 -0700

    Remove CSS file used in background.js

--- public/javascript/background.js
+++ public/javascript/background.js
@@ -4,7 +4,6 @@ chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab_info => {
         active_tab_id = tab.tabId
         if (/^https:\/\/calendar\.google/.test(current_tab_info.url)) {
-            chrome.tabs.insertCSS(null, {file: '../CSS/homepage.css'});
             chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log("Injected foreground.js"));
         }
     });
