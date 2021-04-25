fc410a08f61c61d5a5492265fa28cf4ca78f216d
danielqiang <daniel_qiang@hotmail.com>
2021-04-25 15:25:42 -0700

    oauth prototype v2

--- public/javascript/oauth.js
+++ public/javascript/oauth.js
@@ -1,20 +1,6 @@
 function login() {
-    let authUrl = "https://accounts.google.com/o/oauth2/auth"
-        + '?response_type=token&client_id=' + "770480046288-4olsqcejcdttk1gug1si4i3q3prfl2sb"
-        + '&scope=' + "https://www.googleapis.com/auth/calendar"
-        + '&redirect_uri=' + chrome.identity.getRedirectURL("oauth2");
-
-    console.log(`redirect uri: ${chrome.identity.getRedirectURL("oauth2")}`)
-
-    chrome.identity.launchWebAuthFlow({'url': authUrl, 'interactive': true}, function (redirectUrl) {
-        if (redirectUrl) {
-            console.log(redirectUrl)
-            // let parsed = parse(redirectUrl.substr(chrome.identity.getRedirectURL("oauth2").length + 1));
-            // token = parsed.access_token;
-            // return callback(redirectUrl); // call the original callback now that we've intercepted what we needed
-        } else {
-            return null;
-        }
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+        console.log(token);
     });
 }
 
--- public/javascript/popup.js
+++ public/javascript/popup.js
@@ -1,3 +1,9 @@
+function login() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+        console.log(token);
+    });
+}
+
 document.getElementById("test-OAuth").addEventListener("click", function() {
-    chrome.tabs.create({url: '../HTML/index.html'});
+    login()
 })
