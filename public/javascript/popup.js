822f5b38e360404d7e8cbfcf0fcfec3b7fbf5a46
danielqiang <daniel_qiang@hotmail.com>
2021-04-25 17:04:38 -0700

    working oauth prototype

--- public/javascript/popup.js
+++ public/javascript/popup.js
@@ -1,9 +1,38 @@
-function login() {
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-        console.log(token);
-    });
+function getToken(callback) {
+    chrome.identity.getAuthToken({
+        interactive: true
+    }, callback);
 }
 
-document.getElementById("test-OAuth").addEventListener("click", function() {
-    login()
+function revokeToken() {
+    getToken(function (token) {
+        chrome.identity.removeCachedAuthToken({
+            token: token
+        })
+    })
+}
+
+document.getElementById("test-OAuth").addEventListener("click", function () {
+    getToken(function (token) {
+        console.log(token);
+    });
+})
+
+document.getElementById("test-download-events").addEventListener("click", function () {
+    getToken(
+        function(token) {
+            const headers = new Headers({
+                'Authorization' : 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            })
+
+            const queryParams = { headers };
+
+            fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
+                .then((response) => response.json()) // Transform the data into json
+                .then(function(data) {
+                    console.log(data);
+                })
+        }
+    )
 })
