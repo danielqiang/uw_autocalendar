32cda043ca3daeea0de26374783ca954e4d0500e
Benjamin Lowry <benjaminpaullowry@gmail.com>
2021-05-04 22:11:28 -0700

    Add example tests

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,3 +1,16 @@
 test('basic', () => {
     expect(true).toBe(true);
 });
+
+test('addition', () => {
+    expect(1 + 2).toBe(3);
+});
+
+test('string contains', () => {
+    expect("https://google.com").toMatch(/https/);
+});
+
+const test_array = ['class1', 'class2', 'class3'];
+test('array contains', () => {
+    expect(test_array).toContain('class3');
+});
